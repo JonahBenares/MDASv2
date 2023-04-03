5bb4d7c8c3e9cbbc8bf1080a8618815ca75d13ad
stephinesev <stephineseverino.cenpri@gmail.com>
2023-04-03 15:10:23 +0800

    make region id field readonly and retain data if update in price node

--- storage/framework/views/d7c4da3eab51e2468b827ba39d0900a8.php
+++ storage/framework/views/d7c4da3eab51e2468b827ba39d0900a8.php
@@ -96,7 +96,7 @@
                     </div>
                     <div class="mb-2 w-3/12">
                         <label class="block mb-2 text-sm font-medium text-gray-900 white:text-white">Region ID</label>
-                        <input name="region_id" id='region_id' class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 white:border-gray-600 white:placeholder-gray-400 white:text-white white:focus:ring-blue-500 white:focus:border-blue-500 white:shadow-sm-light" value="<?php echo e($powerplant->region_id); ?>">
+                        <input name="region_id" id='region_id' class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 white:border-gray-600 white:placeholder-gray-400 white:text-white white:focus:ring-blue-500 white:focus:border-blue-500 white:shadow-sm-light" value="<?php echo e($powerplant->region_id); ?>" readonly>
                     </div>
                 </div>
                 <div class="flex justify-between space-x-2">
